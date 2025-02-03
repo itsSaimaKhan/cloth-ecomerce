99440e6f72e66248dfb96b9d765515c59eabf966
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-03 23:52:00 +0500

    main

--- src/components/anouncement.tsx
+++ src/components/anouncement.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
+    <main>
     <div>
-      <h1></h1>
+      <h1>Announcement</h1>
     </div>
+    </main>
   );
 }
