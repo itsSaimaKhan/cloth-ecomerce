6540e8fcaea6a346a51c32fc985bd3314f0181fe
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-11 23:42:44 +0500

    main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import { FaCartArrowDown } from "react-icons/fa";
 
 export default function Header () {
   return (
-    <header>
+    <header className=" w-full h-[60px] flex justify-between items-center ">
         {/* left */}
         <div>
         <h1>SHOP.CO</h1>
@@ -23,16 +23,18 @@ export default function Header () {
             </li>
         </ul>
         {/* search input */}
-        <div>
-            <div>
+      
+            <div className="flex justify-start items-center">
             <FaSearchengin />
             <input placeholder="search item..."></input>
 
 
             </div>
-        </div>
+        
         {/* cart */}
+        <div className="flex items-center">
         <FaCartArrowDown />
+        </div>
 
     </header>
   );
