4bbbf1319a7498fcd9de615f5e383c89a402b885
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-14 23:48:17 +0500

    main

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
      
       size: {
-        default: "h-9 py-2",
+        default: "h-9 pl-2 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md ",
         icon: "h-9 w-9",
