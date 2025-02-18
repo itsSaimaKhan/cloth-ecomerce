312dc6de1662ad9efa946931f2742c8cb88d4d8b
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-18 23:27:29 +0500

    main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,49 +1,49 @@
-import Link from "next/link";
-import { FaSearchengin } from "react-icons/fa";
-import { FaCartArrowDown } from "react-icons/fa";
+// import Link from "next/link";
+// import { FaSearchengin } from "react-icons/fa";
+// import { FaCartArrowDown } from "react-icons/fa";
 
-import { SheetSide } from "./sheet";
-
-
+// import { SheetSide } from "./sheet";
 
 
 
 
-export default function Header () {
-  return (
-    <header className=" w-full h-[60px] bg-white flex justify-between items-center ">
-        {/* left */}
-        <div className="flex justify-center items-center">
-        <SheetSide  />
 
-        <h1 className="text-xl sm:text-3xl font-bold pl-2">SHOP.CO</h1>
-        </div>
-        {/* navbar */}
-        <ul>
-            <li className="space-x-5">
-                <Link href={""}>Shop</Link>
-                <Link href={""}>On Sale</Link>
-                <Link href={""}>New Arrivals</Link>
-                <Link href={""}>Brands</Link>
-            </li>
-        </ul>
-        {/* search input */}
+
+// export default function Header () {
+//   return (
+//     <header className=" w-full h-[60px] bg-white flex justify-between items-center ">
+//         {/* left */}
+//         <div className="flex justify-center items-center">
+//         <SheetSide  />
+
+//         <h1 className="text-xl sm:text-3xl font-bold pl-2">SHOP.CO</h1>
+//         </div>
+//         {/* navbar */}
+//         <ul>
+//             <li className="space-x-5">
+//                 <Link href={""}>Shop</Link>
+//                 <Link href={""}>On Sale</Link>
+//                 <Link href={""}>New Arrivals</Link>
+//                 <Link href={""}>Brands</Link>
+//             </li>
+//         </ul>
+//         {/* search input */}
       
-           <div className="hidden md:block">
-           <div className="flex justify-start items-center w-[330px] h-[40px] rounded-[62px] bg-[#F0F0F0]">
-            <FaSearchengin className="text-xl ml-2"/>
-            <input placeholder="search item..." className="w-full ml-2 outline-none h-full  rounded-[62px] bg-[#F0F0F0]"></input>
+//            <div className="hidden md:block">
+//            <div className="flex justify-start items-center w-[330px] h-[40px] rounded-[62px] bg-[#F0F0F0]">
+//             <FaSearchengin className="text-xl ml-2"/>
+//             <input placeholder="search item..." className="w-full ml-2 outline-none h-full  rounded-[62px] bg-[#F0F0F0]"></input>
 
 
-            </div>
-           </div>
+//             </div>
+//            </div>
         
-        {/* cart */}
-        <div className="flex items-center mr-7 space-x-5">
-        <FaSearchengin className="text-xl ml-2 md:hidden"/>
-        <FaCartArrowDown  className="text-2xl"/>
-        </div>
+//         {/* cart */}
+//         <div className="flex items-center mr-7 space-x-5">
+//         <FaSearchengin className="text-xl ml-2 md:hidden"/>
+//         <FaCartArrowDown  className="text-2xl"/>
+//         </div>
 
-    </header>
-  );
-};
+//     </header>
+//   );
+// };
