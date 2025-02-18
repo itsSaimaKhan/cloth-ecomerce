2dbe0ef102ad6cea355b40864cce6ecd900e0b23
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-18 23:30:13 +0500

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 
-import Announcement from "@/components/anouncement";
-import Header from "@/components/Header";
+// import Announcement from "@/components/anouncement";
+// import Header from "@/components/Header";
 
-export default function Home() {
-  return (
-    <div>
-      <Announcement/>
-      < Header/>
-    </div>
-  );
-};
+// export default function Home() {
+//   return (
+//     <div>
+//       <Announcement/>
+//       < Header/>
+//     </div>
+//   );
+// };
