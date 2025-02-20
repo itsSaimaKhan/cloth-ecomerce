ca35f0d98e59676210c76f798dbb8ba86bfcb57e
itssaimaKhan <samiakhan8754@gmail.com>
2025-02-21 00:00:20 +0500

    main

--- src/components/sheet.tsx
+++ src/components/sheet.tsx
@@ -1,53 +1,53 @@
-// "use client"
+"use client"
 
-// import { Button } from "@/components/ui/button"
-// import { Input } from "@/components/ui/input"
-// import { TiThMenu } from "react-icons/ti";
-// import { Label } from "@/components/ui/label"
-// import {
-//   Sheet,
-//   SheetClose,
-//   SheetContent,
-//   SheetDescription,
-//   SheetFooter,
-//   SheetHeader,
-//   SheetTitle,
-//   SheetTrigger,
-// } from "@/components/ui/sheet"
-// import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { TiThMenu } from "react-icons/ti";
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import Link from "next/link";
 
-// const SHEET_SIDES = [ "left"] as const
+const SHEET_SIDES = [ "left"] as const
 
-// type SheetSide = (typeof SHEET_SIDES)[number]
+type SheetSide = (typeof SHEET_SIDES)[number]
 
-// export function SheetSide() {
-//   return (
-//     <div className="grid  gap-2">
-//       {SHEET_SIDES.map((side) => (
-//         <Sheet key={side}>
-//           <SheetTrigger asChild className="md:hidden">
-//             <Button >
-//            < TiThMenu/>
-//             </Button>
-//           </SheetTrigger>
-//           <SheetContent side={side}>
-//             <SheetHeader>
-//               <SheetTitle>Shop.co</SheetTitle>
+export function SheetSide() {
+  return (
+    <div className="grid  gap-2">
+      {SHEET_SIDES.map((side) => (
+        <Sheet key={side}>
+          <SheetTrigger asChild className="md:hidden">
+            <Button >
+           < TiThMenu/>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Shop.co</SheetTitle>
              
-//             </SheetHeader>
+            </SheetHeader>
             
-//              {/* navbar */}
-//         <ul className="grid grid-cols-1 gap-y-4">
-//             <li className="space-x-5">
-//                 <Link href={""}>Shop</Link>
-//                 <Link href={""}>On Sale</Link>
-//                 <Link href={""}>New Arrivals</Link>
-//                 <Link href={""}>Brands</Link>
-//             </li>
-//         </ul>
-//           </SheetContent>
-//         </Sheet>
-//       ))}
-//     </div>
-//   );
-// };
+             {/* navbar */}
+        <ul className="grid grid-cols-1 gap-y-4">
+            <li className="space-x-5">
+                <Link href={""}>Shop</Link>
+                <Link href={""}>On Sale</Link>
+                <Link href={""}>New Arrivals</Link>
+                <Link href={""}>Brands</Link>
+            </li>
+        </ul>
+          </SheetContent>
+        </Sheet>
+      ))}
+    </div>
+  );
+};
